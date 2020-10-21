cf86e3ae26a957236dec0113ab57610eeec4179d
ardak <ardak.shalkar@gmail.com>
2020-10-21 11:47:51 +0600

    Changed to NodeJS

---
+++ M/server.js
@@ -0,0 +1,4 @@
+const template = `<a href="/sport">Sport news</a> | <a href="/politics">Politic news</a><br/>
+		<a href="/sport/json">Sport news (JSON)</a> | <a href="/politics/json">Politic news (JSON)</a><br/><br/>`;
+let news = {"sport":["C. Ronaldo has scored three goals in last five matches","Golovkin has won match for title"],"politics":["Trump has cancelled his visit to North Corea, because of sanction","N. Nazarbayev has approved new version of alphabet"]};
+
